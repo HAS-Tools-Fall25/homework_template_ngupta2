65d64c5d1866240295f2665151d42fdfbbd6f4ee
Andrew Bennett <bennett.andr@gmail.com>
2025-08-27 17:20:26 -0700

    Fix syntax issues in postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-uv sync
-source ./.venv/bin/activate
-python -m ipykernel install --user --name has_tools
 git clone https://github.com/HAS-Tools-Fall25/CourseMaterials25.git
+uv sync
+source .venv/bin/activate
+python -m ipykernel install --user --name has_tools
